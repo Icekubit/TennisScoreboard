ff57c099073e6948c2e41f0e6af1a521c61ecc5d
icekubit <myznikovkirill@gmail.com>
2023-11-12 18:27:14 +0300

    Add styles

---
+++ src/main/java/io/diegogarcia/icekubit/servlets/StartPageServlet.java
@@ -0,0 +1,17 @@
+package io.diegogarcia.icekubit.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("")
+public class StartPageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/jsp/start-page.jsp").forward(request, response);
+    }
+}
